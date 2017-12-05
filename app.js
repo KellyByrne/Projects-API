b59f2435bbddd2eb1200835f675da7515659c1f9
Kelly Byrne <kellyannbyrne@utexas.edu>
2017-12-05 11:35:19 -0500

    cors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var projects = require('./routes/projects');
 var mongoose = require('mongoose');
-
+var cors = require('cors');
 
 var app = express();
 
@@ -22,6 +22,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors())
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
